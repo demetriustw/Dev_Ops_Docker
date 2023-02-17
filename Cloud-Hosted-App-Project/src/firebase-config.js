682a2f1bfbb29fd23bcb6b7b4b32c3c545ea6327
demetriustw <56927431+demetriustw@users.noreply.github.com>
2023-02-17 14:45:34 -0700

    Update firebase-config.js

--- Cloud-Hosted-App-Project/src/firebase-config.js
+++ Cloud-Hosted-App-Project/src/firebase-config.js
@@ -8,6 +8,7 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// You need to fill in the code in this block underneath
 const firebaseConfig = {
   apiKey: "",
   authDomain: "",
